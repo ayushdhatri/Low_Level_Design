5f553400949a1d1362d027d7ed27b4da0b9dac22
Ayush Solanki <aayushdhatri@gmail.com>
2026-08-15 15:07:14 +0530

    Added enums to movie

---
+++ practice/MovieBooking/enums/SeatType.java
@@ -0,0 +1,19 @@
+package Low_Level_Design.practice.MovieBooking.enums;
+
+public enum SeatType{
+    SILVER(100.00),
+    GOLD(150.00),
+    PLATINUM(200.00),
+    RECLINER(300.0);
+
+    private final double price;
+
+    SeatType(double ticketPrice){
+        this.price = ticketPrice;
+    }
+
+    double getTicketPrice(){
+        return this.price;
+    }
+    
+}
---
+++ practice/MovieBooking/enums/ShowSeatStatus.java
@@ -0,0 +1,7 @@
+package Low_Level_Design.practice.MovieBooking.enums;
+
+public enum ShowSeatStatus {
+    AVAILABLE,
+    HELD,
+    BOOKED
+}
