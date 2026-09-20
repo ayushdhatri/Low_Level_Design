72518ca91101650d9ed2de0e5791c5ecc266c20d
Ayush Solanki <aayushdhatri@gmail.com>
2026-09-20 23:27:41 +0530

    Added Balance Service todo

---
+++ practice/Splitwise/service/BalanceService.java
@@ -0,0 +1,12 @@
+package Low_Level_Design.practice.Splitwise.service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class BalanceService {
+    private final Map<String, Map<String, Double>> balances = new ConcurrentHashMap<>();
+    
+
+
+    
+}
